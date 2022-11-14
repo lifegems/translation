d4d4be5c7224af05f8a85f3a170cf11d7383b016
Joshua Michalik <joshuasmichalik@gmail.com>
2022-11-14 08:22:23 -0500

    Add create page to easily add new items

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 
 // import jsonServerProvider from 'ra-data-json-server';
 // const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import { TranslationCreate } from "./TranslationCreate";
 import { TranslationList } from "./TranslationList";
 import { TranslationEdit } from "./TranslationEdit";
 import Dashboard from "../Dashboard";
 import { CustomMenu } from "../CustomMenu";
 const dataProvider = postgrestRestProvider("/api/admin");
 
+
 const App = () => (
   <Admin dataProvider={dataProvider} dashboard={Dashboard} layout={CustomMenu}>
-    <Resource name="translations" list={TranslationList} edit={TranslationEdit} />
+    <Resource name="translations" list={TranslationList} edit={TranslationEdit} create={TranslationCreate} />
   </Admin>
 );
 
